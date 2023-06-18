dd40665a041b171a6a65af91511ebc287f629119
Mansi <mansins168@gmail.com>
2023-06-18 23:49:47 +0530

    all scenes added

--- admin/src/App.js
+++ admin/src/App.js
@@ -6,15 +6,15 @@ import {Routes, Route} from 'react-router-dom';
 import Dashboard from '../src/scenes/dashboard';
 import Sidebar from './scenes/global/Sidebar';
 import Team from './scenes/team';
-// import Invoices from './scenes/invoices';
+import Invoices from './scenes/invoices';
 import Contacts from './scenes/contacts';
-// import Bar from './scenes/bar';
-// import Form from './scenes/form';
-// import Line from './scenes/line';
-// import Pie from './scenes/pie';
-// import FAQ from './scenes/faq';
-// import Geography from './scenes/geography';
-// import Calendar from './scenes/calendar';
+import Bar from './scenes/bar';
+import Form from './scenes/form';
+import Line from './scenes/line';
+import Pie from './scenes/pie';
+import FAQ from './scenes/faq';
+import Geography from './scenes/geography';
+import Calendar from './scenes/calendar/calendar';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -32,15 +32,15 @@ function App() {
               <Route path="/" element={<Dashboard />} />
                <Route path="/team" element={<Team />} />
 
-              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              <Route path="/invoices" element={<Invoices />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/*<Route path="/bar" element={<Bar />} />
+              <Route path="/bar" element={<Bar />} />
               <Route path="/form" element={<Form />} />
               <Route path="/line" element={<Line />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} />
-              <Route path="/calendar" element={<Calendar />} /> */}
+              <Route path="/calendar" element={<Calendar />} />
             </Routes>
           </main>
         </div>
