edb677e8aa6156b46ec0b58d174592c026e0f1b6
Mansi <mansins168@gmail.com>
2023-06-18 23:29:27 +0530

    team and contacts page added

--- admin/src/App.js
+++ admin/src/App.js
@@ -5,9 +5,9 @@ import Topbar from './scenes/global/Topbar'
 import {Routes, Route} from 'react-router-dom';
 import Dashboard from '../src/scenes/dashboard';
 import Sidebar from './scenes/global/Sidebar';
-// import Team from './scenes/team';
+import Team from './scenes/team';
 // import Invoices from './scenes/invoices';
-// import Contacts from './scenes/contacts';
+import Contacts from './scenes/contacts';
 // import Bar from './scenes/bar';
 // import Form from './scenes/form';
 // import Line from './scenes/line';
@@ -27,13 +27,14 @@ function App() {
           <Sidebar isSidebar={isSidebar} />
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
+            
             <Routes >
               <Route path="/" element={<Dashboard />} />
-              
-              {/* <Route path="/team" element={<Team />} />
-              <Route path="/invoices" element={<Invoices />} />
+               <Route path="/team" element={<Team />} />
+
+              {/* <Route path="/invoices" element={<Invoices />} /> */}
               <Route path="/contacts" element={<Contacts />} />
-              <Route path="/bar" element={<Bar />} />
+              {/*<Route path="/bar" element={<Bar />} />
               <Route path="/form" element={<Form />} />
               <Route path="/line" element={<Line />} />
               <Route path="/pie" element={<Pie />} />
