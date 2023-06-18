68e9fe3aac788d3ed6e3cd321818f86ea21f8616
Mansi <mansins168@gmail.com>
2023-06-18 23:15:57 +0530

    updated

--- admin/src/App.js
+++ admin/src/App.js
@@ -23,11 +23,13 @@ function App() {
   <ColorModeContext.Provider value={colorMode}>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-        <div className="App">
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
           <main className="content">
-          <Topbar setIsSidebar={setIsSidebar} />
+            <Topbar setIsSidebar={setIsSidebar} />
             <Routes >
               <Route path="/" element={<Dashboard />} />
+              
               {/* <Route path="/team" element={<Team />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/contacts" element={<Contacts />} />
